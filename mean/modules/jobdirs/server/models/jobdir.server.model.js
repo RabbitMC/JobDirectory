a6c52a951860185972daa7c837f1db86df3c741a
Miralem Cebic <mail@miralem-cebic.de>
2016-03-28 10:49:28 +0200

    jobs model

--- mean/modules/jobdirs/server/models/jobdir.server.model.js
+++ mean/modules/jobdirs/server/models/jobdir.server.model.js
@@ -16,9 +16,39 @@ var JobdirSchema = new Schema({
     required: 'Please fill Jobdir name',
     trim: true
   },
+  company: {
+    type: String,
+    default: '',
+    trim: true,
+    required: 'Company canot be blank'
+  },
+  description: {
+    type: String,
+    default: '',
+    trim: true
+  },
+  requirements: {
+    type: String,
+    default: '',
+    trim: true
+  },
+  hourly_wage: {
+    type: Number,
+    default: '',
+    trim: true
+  },
+  state: {
+    type: String,
+    default: '',
+  },
+  contact_email: {
+    type: String,
+    default: '',
+    trim: true
+  },    
   created: {
     type: Date,
-    default: Date.now
+    default: Date.now,
   },
   user: {
     type: Schema.ObjectId,
